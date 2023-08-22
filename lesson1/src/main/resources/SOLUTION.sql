5ebb0a093225aa39801cfe853cee49a8e8d07254
IlyaBrutski <101987935+IlyaBrutski@users.noreply.github.com>
2023-08-22 22:04:03 +0300

    SQL 4

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,43 +1,42 @@
-CREATE TABLE Student
-(
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
+CREATE TABLE Student(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR NOT NULL,
     birthday DATE NOT NULL,
-    groupnumber INT NOT NULL
+    groupnumber INT NOT NULL,
+    PRIMARY KEY( id )
 );
 
-CREATE TABLE Subject
-(
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    grade INT NOT NULL
+CREATE TABLE Subject(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    grade INT NOT NULL,
+    PRIMARY KEY( id )
 );
 
-CREATE TABLE PaymentType
-(
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255)
+CREATE TABLE PaymentType(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR NOT NULL,
+    PRIMARY KEY( id )
 );
 
-CREATE TABLE Payment
-(
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Payment(
+    id BIGINT NOT NULL AUTO_INCREMENT,
     type_id BIGINT NOT NULL,
     amount DECIMAL NOT NULL,
     student_id BIGINT NOT NULL,
-    payment_date DATETIME NOT NULL,
-    FOREIGN KEY (type_id) REFERENCES paymentType(id),
-    FOREIGN KEY (student_id) REFERENCES student(id)
+    payment_date TIMESTAMP NOT NULL,
+    PRIMARY KEY( id )
 );
+ALTER TABLE Payment ADD CONSTRAINT PaymentStudentFk FOREIGN KEY (student_id) REFERENCES Student(id);
+ALTER TABLE Payment ADD CONSTRAINT PaymentPaymentTypeFk FOREIGN KEY (type_id) REFERENCES PaymentType(id);
 
-CREATE TABLE Mark
-(
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    student_id BIGINT,
-    subject_id BIGINT,
+CREATE TABLE Mark(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    student_id BIGINT NOT NULL,
+    subject_id BIGINT NOT NULL,
     mark INT NOT NULL,
-    FOREIGN KEY (student_id) REFERENCES Student(id),
-    FOREIGN KEY (subject_id) REFERENCES Subject(id)
+    PRIMARY KEY( id )
 );
-
+ALTER TABLE Mark ADD CONSTRAINT MarkStudentFk FOREIGN KEY (student_id) REFERENCES Student(id);
+ALTER TABLE Mark ADD CONSTRAINT MarkSubjectFk FOREIGN KEY (subject_id) REFERENCES Subject(id);
